72126277bf7b2c13191a3c98be2122a6eec41c60
freeba21 <93439880+freeba21@users.noreply.github.com>
2021-10-30 18:21:17 +0200

    d

--- sk/uniba/fmph/dcs/Game.java
+++ sk/uniba/fmph/dcs/Game.java
@@ -15,7 +15,7 @@ public class Game {
     Play play;
     TurnStatus turnStatus;
     int emptyDeck;
-
+    LinkedList<GameCardType> type;
     public Game(int empty_decks,int market, int estate, int copper, int smithy, int village, int festival, int laboratory){
         if(market<5) market=5; if(smithy<5) smithy=5;
         if (estate<5) estate=5; if(village<5) village=5;
@@ -29,23 +29,49 @@ public class Game {
         hand=new Hand(deck);
         turnStatus=new TurnStatus();
         turn=new Turn();
-
-
+        type=new LinkedList<>();
+        type.add(GameCardType.GAME_CARD_TYPE_ESTATE);
+        type.add(GameCardType.GAME_CARD_TYPE_VILLAGE);
+        type.add(GameCardType.GAME_CARD_TYPE_COPPER);
+        type.add(GameCardType.GAME_CARD_TYPE_MARKET);
+        type.add(GameCardType.GAME_CARD_TYPE_FESTIVAL);
+        type.add(GameCardType.GAME_CARD_TYPE_LABORATORY);
+        type.add(GameCardType.GAME_CARD_TYPE_SMITHY);
+        buyPhase=false;
+        actionPhase=true;
     }
 
     public boolean playCard(int handIdx) {
         if(!actionPhase)
         {System.err.println("Nie je ActionPhase, nie je možné hrať.");
             return false;}
-
+        if(turnStatus.getActions()>0){
+            return false;
+        }
+        else System.err.println("Nedostatok akcií.");
         return true;
     }
 
     public boolean endPlayCardPhase() {
+        if(buyPhase){
+            buyPhase=false;
+            actionPhase=true;
+            endTurn();
+        }else if(actionPhase){
+            actionPhase=false;
+            buyPhase=true;
+        }
         return false;
     }
 
     public boolean buyCard(int buyCardIdx) {
+        if(!buyPhase){
+            System.err.println("Nie je BuyPhase, nie je možné hrať.");
+            return false;
+        }if(turnStatus.getBuys()<1){
+            System.err.println("Nie je možný nákup karty pre nedostatočný buys.");
+            return false;
+        }
         return false;
     }
 
